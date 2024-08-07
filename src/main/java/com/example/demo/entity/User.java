1225523e8b69c6130e1c99bacf8d011e64ba7363
mohammad <masd9617@gmail.com>
2024-08-07 14:13:55 +0330

    change oracle to h2 database

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -12,7 +12,7 @@ import java.util.Date;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String family;
